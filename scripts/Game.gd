2c944cc8e6e41bef25fbe7f47b5e55e0779eacd5
SleepingRobot <ross.srk@gmail.com>
2018-09-24 23:25:10 -0700

    Adds support for correctly processing level JSON files

--- scripts/Game.gd
+++ scripts/Game.gd
@@ -2,10 +2,15 @@ extends Node
 
 onready var global = get_node("/root/Global")
 onready var wave_timer = $Timer
-var number_of_waves = 6
+var number_of_waves = 0
+var wave_id = 0
 var waves
-var new_wave = false
-var wave_counter = 0
+var spawn_new = false
+var waves_over = false
+var current_wave = 0
+var current_enemy_batch = 0
+var current_enemy = 0
+var enemy_quantity = 0
 export(PackedScene) var enemy
 
 
@@ -13,48 +18,53 @@ func _ready():
 	#if global.restart:
 		#get_tree().reload_current_scene()
 	#wave_timer.stop()
+	_load_level("Level0001")
 	global.game = self
 
 
 func _process(delta):
-	if new_wave:
-		_load_level("Level0001")
-		_begin_wave(randi()%2+1, rand_range(.1,.5))
+	if spawn_new and not waves_over:
+		_process_wave()
 		
 		
 func _load_level(level):
 	var file = File.new()
 	file.open("res://Config/Levels/" + level + ".json", File.READ)
 	var text = file.get_as_text()
-	print("LEVEL: " + text)
 	waves = JSON.parse(text).result
-	print(waves)
-	number_of_waves = waves["waves"].size()
-	print("WAVE LENGTH: " + str(number_of_waves))
-
-func _begin_wave(path_num, set_timer):
-	_spawn_enemy(enemy,"Path1")
-	_spawn_enemy(enemy,"Path2")
-	# path_num is the path number the enemy will spawn on and follow
-	# set_timer sets the time between enemy waves
-	_spawn_enemy(enemy,"Path" + str(path_num))
-	new_wave = false
-	wave_timer.set_wait_time(set_timer)
+	if (waves["waves"].size() > 0):
+		spawn_new = true
+	
+	
+func _process_wave():
+	_spawn_enemy(waves["waves"][current_wave]["enemies"][current_enemy_batch])
+	spawn_new = false
+	wave_timer.set_wait_time(waves["waves"][current_wave]["enemies"][current_enemy_batch].enemy_timer)
 	wave_timer.start()
 	
-
-func _spawn_enemy(type,path):
-	var new_enemy = type.instance()
-	new_enemy.init("Enemy",200,125,20)
+	
+func _spawn_enemy(d):
+	var new_enemy = enemy.instance()
+	new_enemy.init(d.sprite ,d.speed,d.health,d.damage)
 	var pathFollow = PathFollow2D.new()
 	pathFollow.set_loop(false)
-	self.get_node(path).add_child(pathFollow)
+	self.get_node(d.path).add_child(pathFollow)
 	pathFollow.add_child(new_enemy)
+	
 
+func _increment_enemy():
+	current_enemy += 1
+	if(current_enemy > waves["waves"][current_wave]["enemies"][current_enemy_batch].quantity - 1):
+		current_enemy = 0
+		current_enemy_batch += 1
+		if(current_enemy_batch > waves["waves"][current_wave]["enemies"].size() - 1):
+			current_enemy_batch = 0
+			current_wave += 1
+			if(current_wave > waves["waves"].size() - 1):
+				current_wave = 0
+				waves_over = true
 
 func _on_Timer_timeout():
-	wave_counter += 1
-	if wave_counter <= number_of_waves:
-		new_wave = true
-	else:
-		new_wave = false
+	_increment_enemy()
+	if not waves_over:
+		spawn_new = true
