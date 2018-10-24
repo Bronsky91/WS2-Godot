26bb1fbdbb57c12e6e37560f881c135cb1ca0b0a
SleepingRobot <ross.srk@gmail.com>
2018-10-24 02:27:50 -0700

    Fixes bugs with pulsar rune
    Programmatically instantiates new collision child to remove editor shared resource bug. Corrects beam scaling and collision sizing logic

--- Scripts/Pulsar.gd
+++ Scripts/Pulsar.gd
@@ -1,38 +1,51 @@
 extends "res://Scripts/Rune.gd"
 
-onready var rune_range = $Area2D/CollisionShape2D
-onready var beam = $Sprite2
-
-var next_pattern = Vector2(0,0)
+onready var beam = $Beam
+onready var beam_area = $Beam/Area2D
+onready var beam_sprite = $Beam/Area2D/Sprite
 var pattern = []
-var scales = []
 var enemies
 var step = 0
+var beam_offset = 0
 
 
 func _ready():
-	pattern = [Vector2(0,-_range), Vector2(_range, 0), Vector2(0, _range), Vector2(-_range,0)]
-	scales = [Vector2(5,1), Vector2(1,5), Vector2(5,1), Vector2(1,5)]
-	
+	pattern = [Vector2(0,90), Vector2(-90, 0), Vector2(0, -90), Vector2(90,0)]
+	var collision = CollisionShape2D.new()
+	var shape = RectangleShape2D.new()
+	resize_sprite(beam_sprite, _range)
+	resize_shape(shape, beam_sprite)
+	beam_offset = sprite_height_offset(beam_sprite)
+	beam_area.position = Vector2( 0, beam_offset )
+	collision.set_shape(shape)
+	beam_area.add_child(collision)
+	pulse_timer.set_wait_time(_speed)
+	pulse_timer.start()
+
 
 func _process(delta):
 	if step <= 3 and not firing:
-		pulse_timer.set_wait_time(_speed)
-		pulse_timer.start()
 		firing = true
-		#rune_range.set_scale(scales[step])
-		#rune_range.shape.b = Vector2(pattern[step])
-		var direction = (pattern[step] - rune_range.offset()).normalized()
+		var direction = (pattern[step] - Vector2(0,0)).normalized()
 		var rad_angle = atan2(-direction.x, direction.y)
-		rune_range.shape.set_rotation(rad_angle)
-		rune_range.set_position(rune_range.shape.a + (direction * _speed * delta))
-		print(rune_range.shape.extents)
 		beam.set_rotation(rad_angle)
-		beam.set_position(rune_range.shape.a + (direction * _speed * delta))
 		step += 1
 		if step == 4:
 			step = 0
-	
+
+
+func resize_sprite(sprite, height):
+	sprite.scale = Vector2(sprite.scale.x, height / sprite.texture.get_height())
+
+
+func resize_shape(shape, sprite):
+	var size = sprite.texture.get_size() * sprite.get_scale()
+	shape.set_extents(size/2)
+
+
+func sprite_height_offset(sprite):
+	return ( (sprite.texture.get_size() * sprite.get_scale()) / 2 ).y
+
 
 func _on_PulseTimer_timeout():
 	rearm()
@@ -42,4 +55,4 @@ func _on_Area2D_body_entered(body):
 	enemies = get_tree().get_nodes_in_group("enemies")
 	if body in enemies:
 		body.take_damage(_damage)
-		print('entered damage')
+		enemies = null
