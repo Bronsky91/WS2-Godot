755a29197dc7fc24b06d2c8b38a9c35957c3c147
Bryan Reed <bryan87reed@gmail.com>
2018-10-23 21:30:25 -0700

    Refactors NecroSigil to use to Area2D collisions

--- Scripts/NecroSigil.gd
+++ Scripts/NecroSigil.gd
@@ -2,15 +2,29 @@ extends "res://Scripts/Rune.gd"
 
 export(PackedScene) var minion
 
+onready var rune_range = $Area2D/CollisionShape2D
+
+var enemies
+
 func _ready():
-	pass
+	rune_range.shape.radius = _range
+	
 
 func _process(delta):
-	time += delta #
-	target = choose_target()
-	if target != null and not target.get_ref().is_connected('died', self, 'on_target_died'):
-		target.get_ref().connect('died', self, 'on_target_died')
+	pass
 			
 			
-func on_target_died():
-	_summon(minion, _mob_stats, target.get_ref().position)
+func on_target_died(body):
+	_summon(minion, _mob_stats, body.position)
+
+
+func _on_Area2D_body_entered(body):
+	enemies = get_tree().get_nodes_in_group("enemies")
+	if body in enemies and not body.is_connected('died', self, 'on_target_died'):
+		body.connect('died', self, 'on_target_died', [body])
+
+
+func _on_Area2D_body_exited(body):
+	enemies = get_tree().get_nodes_in_group("enemies")
+	if body in enemies and body.is_connected('died', self, 'on_target_died'):
+		body.disconnect('died', self, 'on_target_died')
